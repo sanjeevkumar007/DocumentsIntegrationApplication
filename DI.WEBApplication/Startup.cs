7e393d61a822e5ae71e13b7efd7e979667f851d1
san.ank007 <san.ank007@gmail.com>
2021-03-24 22:49:45 +0530

    DI.WEBApp Configured basic mvc application from starup class added bootstrap and jquery library Added Home controller with view Working fine.

--- DI.WEBApplication/Startup.cs
+++ DI.WEBApplication/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +13,19 @@ namespace DI.WEBApplication
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +36,19 @@ namespace DI.WEBApplication
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute
+                (
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                
+                );
+
             });
         }
     }
