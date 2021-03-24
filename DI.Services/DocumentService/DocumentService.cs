1ae9c7861a054619972a279fbe90cdc2e70dbcf7
san.ank007 <san.ank007@gmail.com>
2021-03-24 22:21:53 +0530

    DI.Services Added IDocument Service Added Document Service with implmentation for next inner level generic repository.

---
+++ DI.Services/DocumentService/DocumentService.cs
@@ -0,0 +1,44 @@
+﻿using DI.Domain.Entities;
+using DI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI.Services.DocumentService
+{
+    public class DocumentService : IDocumentService
+    {
+        private readonly IGRepository<Document> _repository;
+
+        public DocumentService(IGRepository<Document> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> AddDocumentAsync(Document document)
+        {
+            return await _repository.AddedAsync(document);
+        }
+
+        public async Task<List<Document>> GetAllDocumentsAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<Document> GetDocumentByIdAsync(int Id)
+        {
+            return await _repository.GetByIdAsync(Id);
+        }
+
+        public async Task<bool> RemoveDocumentAsync(int Id)
+        {
+            return await _repository.RemoveAsync(Id);
+        }
+
+        public async Task<bool> UpdateDocumentAsync(int Id, Document document)
+        {
+            return await _repository.UpdateAsync(Id, document);
+        }
+    }
+}
---
+++ DI.Services/DocumentService/IDocumentService.cs
@@ -0,0 +1,17 @@
+﻿using DI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI.Services.DocumentService
+{
+    public interface IDocumentService
+    {
+        Task<List<Document>> GetAllDocumentsAsync();
+        Task<Document> GetDocumentByIdAsync(int Id);
+        Task<bool> AddDocumentAsync(Document document);
+        Task<bool> UpdateDocumentAsync(int Id, Document document);
+        Task<bool> RemoveDocumentAsync(int Id);
+    }
+}
