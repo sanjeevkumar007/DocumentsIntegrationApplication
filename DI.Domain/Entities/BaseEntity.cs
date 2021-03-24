e342d3a7e2afc89bd1e7829a59e0ed5f8b5e2f75
san.ank007 <san.ank007@gmail.com>
2021-03-24 21:20:56 +0530

    DI Entities Added Base Entity and basic Document class

---
+++ DI.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string DocumentId { get; set; }
+        public DateTimeOffset AddedOn { get; set; }
+        public DateTimeOffset ModifiedOn { get; set; }
+    }
+}
---
+++ DI.Domain/Entities/Document.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI.Domain.Entities
+{
+    public class Document:BaseEntity
+    {
+        public string DocumentName { get; set; }
+        public string PersonName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string DocumentImageUrl { get; set; }
+
+    }
+}
